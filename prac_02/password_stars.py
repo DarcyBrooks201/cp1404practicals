f24a1d843e94180360e37bb1e47463d8d4ac6d96
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-09-26 11:15:18 +1000

    Add module docstring

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,9 @@
-min_password_length = int(input("minimum length: "))
-password = input("Password: ")
-while len(password) < min_password_length:
-    print("Password must be longer")
+"""Create a string of stars based on length of password"""
+
+def main():
+    min_password_length = int(input("minimum length: "))
     password = input("Password: ")
-print('*' * len(password))
+    while len(password) < min_password_length:
+        print("Password must be longer")
+        password = input("Password: ")
+    print('*' * len(password))
