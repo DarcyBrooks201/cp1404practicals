9554cdd8b7f7550a27134cbf4f3ebaf749454176
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-09-26 11:21:29 +1000

    Add week 2 work

---
+++ prac_02/seminar_02.py
@@ -0,0 +1,36 @@
+# import random
+
+#
+# low_number = int(input("Low: "))
+# high_number = int(input("high: "))
+# while high_number <= low_number:
+#     print("must be higher")
+#     high_number = int(input("high: "))
+# random_number = random.randint(low_number, high_number)
+# print(':)' * random_number)
+
+
+"""Commit after being checked with Pylint"""
+"""Add module docstring"""
+
+
+"""add is_even function"""
+
+def is_even(number):
+    return number % 2 == 0
+
+"""Simplify is_even function"""
+
+print(is_even(5))
+print(is_even(6))
+
+"""Test is_even function"""
+
+"""
+determine_subject_grade
+convert_usd_to_aud
+print_report
+caclulate_average
+is_even
+get_valid_age
+"""
