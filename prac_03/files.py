aeee5bbd63c377f8ab75ceec3eed2f53a44b2dfd
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-10-03 15:40:13 +1000

    Answer question 4 of files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,16 +1,16 @@
 """CP1404 Prac 3 Do-From_Scratch exercise files.py"""
 # 1
-# name = input("name: ")
-# out_file = open("name.txt", "w")
-# print(name, file=out_file)
-# out_file.close()
+name = input("name: ")
+out_file = open("name.txt", "w")
+print(name, file=out_file)
+out_file.close()
 #
 # # 2
-# in_file = open("name.txt", "r")
-# for line in in_file:
-#     name = line.strip("\n")
-# in_file.close()
-# print(f"Hi {name}!")
+in_file = open("name.txt", "r")
+for line in in_file:
+    name = line.strip("\n")
+in_file.close()
+print(f"Hi {name}!")
 
 # 3
 total_number = 0
@@ -19,4 +19,12 @@ with open("numbers.txt") as in_file:
         if i < 2:
             number = int(line.strip())
             total_number += number
+print(total_number)
+
+# 4
+total_number = 0
+with open("numbers.txt") as in_file:
+    for line in in_file:
+        number = int(line.strip())
+        total_number += number
 print(total_number)
