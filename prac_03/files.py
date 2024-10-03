9d850a0736da0f07510a5bb1babdc00dc5be67ff
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-10-03 15:48:48 +1000

    Edit answer to question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,8 +4,8 @@ name = input("name: ")
 out_file = open("name.txt", "w")
 print(name, file=out_file)
 out_file.close()
-#
-# # 2
+
+# 2
 in_file = open("name.txt", "r")
 line = in_file.readline()
 name = line.strip("\n")
@@ -13,13 +13,10 @@ in_file.close()
 print(f"Hi {name}!")
 
 # 3
-total_number = 0
 with open("numbers.txt") as in_file:
-    for i, line in enumerate(in_file):
-        if i < 2:
-            number = int(line.strip())
-            total_number += number
-print(total_number)
+    number1 = int(in_file.readline())
+    number2 = int(in_file.readline())
+print(number1 + number2)
 
 # 4
 total_number = 0
