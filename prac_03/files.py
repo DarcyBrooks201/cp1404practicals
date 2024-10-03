b6ee3c88b5db2734d1cead6a489877924970c7ca
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-10-03 15:05:02 +1000

    Answer question 2 for files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,5 +5,12 @@ out_file = open("name.txt", "w")
 print(name, file=out_file)
 out_file.close()
 
+# 2
+in_file = open("name.txt", "r")
+for line in in_file:
+    name = line.strip("\n")
+in_file.close()
+print(f"Hi {name}!")
 
+# 3
 
