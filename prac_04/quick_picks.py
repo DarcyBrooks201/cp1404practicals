74bfe0f72de76c00c73745182f24bca20fbd7cb2
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-10-11 13:58:58 +1000

    Finish quick_picks.py

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,7 +8,10 @@ number_of_quick_picks = int(input("How many quick picks? "))
 for i in range(number_of_quick_picks):
     random_numbers = []
     while len(random_numbers) < NUMBER_OF_RANDOM_NUMBERS:
-        random_numbers.append(random.randint(MINIMUM_RANDOM, MAXIMUM_RANDOM))
+        random_number = random.randint(MINIMUM_RANDOM, MAXIMUM_RANDOM)
+        while random_number in random_numbers:
+            random_number = random.randint(MINIMUM_RANDOM, MAXIMUM_RANDOM)
+        random_numbers.append(random_number)
     random_numbers.sort()
-    print(random_numbers)
+    print(" ".join(f"{number:2}" for number in random_numbers))
 
