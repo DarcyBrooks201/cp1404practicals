3d675de494e23de41836cc6f637d2238b7fd9ae1
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-10-11 13:05:54 +1000

    Remove comment

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,18 +1,18 @@
 """Cp1404 Prac 4 lists_exercises file"""
 
-# numbers = []
-# total = 0
-# for i in range(5):
-#     number = int(input("Number: "))
-#     numbers.append(number)
-# print(f"The first number is {numbers[0]} ")
-# print(f"The last number is {numbers[-1]} ")
-# print(f"The smallest number is {min(numbers)} ")
-# print(f"The largest number is {max(numbers)} ")
-# for number in numbers:
-#     total += number
-# average = total / len(numbers)
-# print(f"The average of the numbers is {average:.2} ")
+numbers = []
+total = 0
+for i in range(5):
+    number = int(input("Number: "))
+    numbers.append(number)
+print(f"The first number is {numbers[0]} ")
+print(f"The last number is {numbers[-1]} ")
+print(f"The smallest number is {min(numbers)} ")
+print(f"The largest number is {max(numbers)} ")
+for number in numbers:
+    total += number
+average = total / len(numbers)
+print(f"The average of the numbers is {average:.2} ")
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
