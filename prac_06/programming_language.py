27ad64acf6df8b22c507a420a7273a2cfe73bf6d
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-10-31 11:52:15 +1000

    Finish programming_language.py and languages.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,11 +2,19 @@
 
 
 class ProgrammingLanguage:
-    def __init__(self, typing="Dynamic", reflection="Yes", year=1991):
-        """Construct typing, reflection and year"""
+    """Class ProgrammingLanguage that takes name, typing, reflection and year"""
+
+    def __init__(self, name="", typing="Dynamic", reflection="Yes", year=1991):
+        """Construct name, typing, reflection and year"""
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
+    def __repr__(self):
+        """Return string of name, typing, reflection and year"""
+        return f"{self.name}, {self.typing}, {self.reflection}, {self.year}"
+
     def is_dynamic(self):
+        """Determine if a typing style is dynamic"""
         return self.typing == "Dynamic"
