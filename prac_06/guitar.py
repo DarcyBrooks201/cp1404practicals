4287a877af6858f35112ca5b7f7fdc8dd7655127
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-11-01 10:42:50 +1000

    Reformat guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,6 +4,7 @@
 Expected 20 minutes, actually took 37
 """
 
+
 class Guitar:
     """Guitar class that takes name, year and cost"""
 
@@ -20,7 +21,7 @@ class Guitar:
     def get_age(self):
         """Gets how old the guitar is based on current year"""
         current_year = 2022
-        return int(2022 - self.year)
+        return int(current_year - self.year)
 
     def is_vintage(self):
         """Returns True if the guitar is over 50 years old"""
@@ -28,13 +29,10 @@ class Guitar:
         return age > 50
 
 
-
-
-
 if __name__ == '__main__':
     guitars = []
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     print("My guitars!")
     new_guitar_name = input("Name: ")
     while new_guitar_name != "":
