ba053961b9f2c0efb7df69e580df0559830c9871
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-11-01 10:37:49 +1000

    Add basic testing to guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,5 +1,6 @@
 """prac 6 guitar.py
-9:58 -
+9:58 - 10:25
+10:31 -
 Expected 20 minutes, actually
 """
 
@@ -27,3 +28,26 @@ class Guitar:
         return age > 50
 
 
+
+
+
+if __name__ == '__main__':
+    guitars = []
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    print("My guitars!")
+
+    new_guitar_name = input("Name: ")
+    new_guitar_year = int(input("Year: "))
+    new_guitar_cost = float(input("Cost: $"))
+    new_guitar = Guitar(new_guitar_name, new_guitar_year, new_guitar_cost)
+    guitars.append(new_guitar)
+    print(f"{Guitar.__str__(new_guitar)} added.")
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        if Guitar.is_vintage(guitar):
+            vintage_string = "(vintage)"
+        else:
+            vintage_string = ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
