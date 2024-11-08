c81dfe3034a22cc29cd73ce3024618997c1ecbaa
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-11-09 08:42:02 +1000

    Start Project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,1 +1,15 @@
 """prac 7, project class"""
+
+
+class Project:
+    def __init__(self, name="", start_date="31/12/2000", priority=0, cost_estimate=0.0, completion_percentage=0.0):
+        """Construct project object from Name, Start Date, Priority, Cost Estimate, and Completion Percentage"""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __repr__(self):
+        """String version of project object"""
+        return f"{self.name}    {self.start_date}   {self.priority} {self.cost_estimate}    {self.completion_percentage}"
