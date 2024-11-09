c32251f0db48d2dd5de04e46e4d55e9cd3078b57
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-11-09 10:53:44 +1000

    Complete update project and add project functions

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,13 +6,39 @@ class Project:
         """Construct project object from Name, Start Date, Priority, Cost Estimate, and Completion Percentage"""
         self.name = name
         self.start_date = start_date
-        self.priority = priority
-        self.cost_estimate = cost_estimate
-        self.completion_percentage = completion_percentage
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
 
     def __repr__(self):
         """String version of project object"""
         return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate ${self.cost_estimate}, completion: {self.completion_percentage}%"
+
+    def __getitem__(self, key):
+        """Return part of the object"""
+        if key == "name":
+            return self.name
+        elif key == "start_date":
+            return self.start_date
+        elif key == "priority":
+            return self.priority
+        elif key == "cost_estimate":
+            return self.cost_estimate
+        elif key == "completion_percentage":
+            return self.completion_percentage
+
+    def __setitem__(self, key, value):
+        """Change part of the object"""
+        if key == "name":
+            self.name = value
+        elif key == "start_date":
+            self.start_date = value
+        elif key == "priority":
+            self.priority = value
+        elif key == "cost_estimate":
+            self.cost_estimate = value
+        elif key == "completion_percentage":
+            self.completion_percentage = value
 
     def project_original_string(self):
         """Original string version of project"""
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,7 +2,8 @@
 prac 7 project_management
 expected time: 60 minutes
 8:32 - 9:27
-9:50 -
+9:50 - 10:07
+10:24 -
 Expected 60 minutes, was actually ()
 """
 
@@ -23,30 +24,64 @@ def main():
         if menu_choice == "L":
             filename = input("Filename: ")
             projects = load_file(filename)
+            menu_choice = input(">>> ").upper()
         elif menu_choice == "S":
             filename = input("Filename: ")
             save_file(filename, projects)
+            menu_choice = input(">>> ").upper()
         elif menu_choice == "D":
-            # TODO fix
+            # TODO add sorting
+            # projects.sort(project[2])
             print("Incomplete projects:")
-            print(project for project in projects if project[4] == 100)
+            for project in projects:
+                if project["completion_percentage"] != 100:
+                    print(project)
             print("Complete projects:")
-            print(project for project in projects if project[4] != 100)
+            for project in projects:
+                if project["completion_percentage"] == 100:
+                    print(project)
+            menu_choice = input(">>> ").upper()
         elif menu_choice == "F":
+            #TODO complete
             pass
+            menu_choice = input(">>> ").upper()
         elif menu_choice == "A":
-            name = input("name: ")
-            start_date = input("start date: ")
-            priority = int(input("priority: "))
-            cost_estimate = float(input("cost estimate: "))
-            completion_percentage = int(input("completion percentage: "))
-            projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
+            add_project(projects)
+            menu_choice = input(">>> ").upper()
         elif menu_choice == "U":
-            pass
+            update_project(projects)
+            menu_choice = input(">>> ").upper()
     save_choice = input(f"Would you like to save to {filename}?").upper()
     if save_choice == "Y":
         save_file(filename, projects)
     print("Thank you for using custom-built project management software.")
+
+
+def update_project(projects):
+    project_number = 0
+    for project in projects:
+        print(f"{project_number} {project}")
+        project_number += 1
+    project_choice = int(input("Project choice"))
+    while project_choice < 0 or project_choice > len(projects):
+        print("Invalid project choice")
+        project_choice = int(input("Project choice"))
+    print(projects[project_choice])
+    new_percentage = int(input("new percentage: "))
+    if new_percentage != "":
+        projects[project_choice][4] = new_percentage
+    new_priority = int(input("new priority: "))
+    if new_priority != "":
+        projects[project_choice][2] = new_priority
+
+
+def add_project(projects):
+    name = input("name: ")
+    start_date = input("start date: ")
+    priority = int(input("priority: "))
+    cost_estimate = float(input("cost estimate: "))
+    completion_percentage = int(input("completion percentage: "))
+    projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
 
 
 def display_menu():
@@ -73,6 +108,7 @@ def save_file(filename, projects):
     out_file = open(filename, "w")
     print("Name	Start Date	Priority	Cost Estimate	Completion Percentage", file=out_file)
     for project in projects:
+        #TODO fix output
         print(Project.project_original_string(project), file=out_file)
     out_file.close()
     print(f"Successfully saved {len(projects)} projects to {filename}")
