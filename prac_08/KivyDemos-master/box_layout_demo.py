26e2c6a7264996f2ea86edc0b6ac8c1e7ed04ce0
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-11-19 11:01:17 +1000

    Add docstrings to functions in convert_miles_km.py and box_layout_demo.py

--- prac_08/KivyDemos-master/box_layout_demo.py
+++ prac_08/KivyDemos-master/box_layout_demo.py
@@ -4,15 +4,18 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Build the kivy GUI"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Greet the name entered"""
         print("test")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clear fields to original state"""
         self.root.ids.output_label.text = "Greet"
         self.root.ids.input_name.text = ""
 
