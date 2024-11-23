92099f83e4166330fbca052ef5f2ccb2e6778ae2
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-11-23 13:08:09 +1000

    Fix string formating for taxi_test.py and silver_service_taxi_test.py

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -3,7 +3,7 @@ from taxi import Taxi
 
 t1 = Taxi("Prius 1", 100)
 t1.drive(40)
-print(f"{t1}, current fare: {t1.get_fare()}")
+print(f"{t1}, current fare: ${t1.get_fare():.2f}")
 t1.start_fare()
 t1.drive(100)
-print(f"{t1}, current fare: {t1.get_fare()}")
+print(f"{t1}, current fare: ${t1.get_fare():.2f}")
