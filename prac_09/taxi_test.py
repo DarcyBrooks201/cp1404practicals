d54129691ab18a297700740738198b2d1f92a08c
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-11-23 12:33:27 +1000

    Add taxi.py and taxi_test.py

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,9 @@
+"""CP1404 prac 9 taxi class test"""
+from taxi import Taxi
+
+t1 = Taxi("Prius 1", 100, 1.23)
+t1.drive(40)
+print(f"{t1}, current fare: {t1.get_fare()}")
+t1.start_fare()
+t1.drive(100)
+print(f"{t1}, current fare: {t1.get_fare()}")
