e4443be19aeda9d38893ab3a61b7ad370b816f73
Firebuster2010 <brooksdarcy2010@gmail.com>
2024-11-23 12:36:06 +1000

    Modify taxi.py to have price_per_km as a class variable

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,7 +1,7 @@
 """CP1404 prac 9 taxi class test"""
 from taxi import Taxi
 
-t1 = Taxi("Prius 1", 100, 1.23)
+t1 = Taxi("Prius 1", 100)
 t1.drive(40)
 print(f"{t1}, current fare: {t1.get_fare()}")
 t1.start_fare()
